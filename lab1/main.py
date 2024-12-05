bfdb595df56fe0540a75bc1296e28d1209899d9e
AndreyMinyaylo <113465922+AndreyMinyay1o@users.noreply.github.com>
2024-12-05 08:20:02 +0300

    Построение класса для сущности Читатель(Reader)

--- lab1/main.py
+++ lab1/main.py
@@ -11,12 +11,33 @@ class Book:
         self._isbn = isbn
         self._genre = genre
 
+# Класс Reader (Читатель)
 class Reader:
     def __init__(self, reader_id, name, address, phone):
         self._reader_id = reader_id
         self._name = name
         self._address = address
         self._phone = phone
+        self._validate_fields()
+
+    def _validate_field(self, value, expected_type, error_message, min_length=None):
+        if not isinstance(value, expected_type):
+            raise ValueError(error_message)
+        if min_length and len(value) < min_length:
+            raise ValueError(error_message)
+
+    def _validate_phone(self, phone):
+        if not re.match(r"^\+?[0-9]{10,15}$", phone):
+            raise ValueError("Phone number must be in a valid format.")
+    
+    def _validate_fields(self):
+        self._validate_field(self._reader_id, int, "Reader ID must be a valid integer.")
+        self._validate_field(self._name, str, "Name must be at least 3 characters long.", min_length=3)
+        self._validate_field(self._address, str, "Address must be at least 5 characters long.", min_length=5)
+        self._validate_phone(self._phone)
+
+    def __str__(self):
+        return f"Reader(ID: {self._reader_id}, Name: {self._name}, Address: {self._address}, Phone: {self._phone})"
 
 class Borrow:
     def __init__(self, borrow_id, book_id, reader_id, date_borrowed, date_returned):
